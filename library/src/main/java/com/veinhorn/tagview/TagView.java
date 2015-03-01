c7e47e783d0573f53defd45e055df47c83fb66a3
VEINHORN <mcklas@mail.ru>
2015-03-01 20:50:04 +0300

    Update example, add new properties for TagView, add new type of tagview.

--- library/src/main/java/com/veinhorn/tagview/TagView.java
+++ library/src/main/java/com/veinhorn/tagview/TagView.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
@@ -22,37 +23,45 @@ public class TagView extends TextView {
     private int rightPadding = 15;
     private int topPadding = 10;
     private int bottomPadding = 10;
-    private static final int PADDING = 10;
+
+    private static final int TEXT_COLOR_DEFAULT = Color.WHITE;
+    private static final int CIRCLE_COLOR_DEFAULT = Color.WHITE;
+    private static final int BORDER_RADIUS_DEFAULT = 5;
+    private static final int CIRCLE_RADIUS_DEFAULT = 7;
 
     private Paint backgroundPaint;
     private Paint circlePaint;
+    private Paint trianglePaint;
 
-    private float borderRadius = 5;
-    private float circleRadius = 5;
-
-    private int tagType = 0;
+    // TagView properties
+    private int tagType;
+    private int tagColor;
+    private boolean tagUpperCase;
+    private float tagBorderRadius;
+    private float tagCircleRadius;
+    private int tagCircleColor;
+    private int tagTextColor;
+    //////////////////
 
     public static final int CLASSIC = 0;
     public static final int MODERN = 1;
-
-    private static int TEXT_COLOR = Color.WHITE;
+    public static final int TRAPEZIUM = 2;
+    public static final int MODERN_TRAPEZIUM = 3;
 
     private class TagDrawable extends Drawable {
         @Override
-        public void setAlpha(int alpha) {
-
-        }
+        public void setAlpha(int alpha) {}
 
         @Override
         public void draw(Canvas canvas) {
             if(tagType == CLASSIC) drawClassicTag(getBounds(), canvas);
             else if(tagType == MODERN) drawModernTag(getBounds(), canvas);
+            else if(tagType == TRAPEZIUM) drawTrapeziumTag(getBounds(), canvas);
+            else if(tagType == MODERN_TRAPEZIUM) drawModernTrapeziumTag(getBounds(), canvas);
         }
 
         @Override
-        public void setColorFilter(ColorFilter colorFilter) {
-
-        }
+        public void setColorFilter(ColorFilter colorFilter) {}
 
         @Override
         public int getOpacity() {
@@ -66,6 +75,12 @@ public class TagView extends TextView {
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.TagView, 0, 0);
         try {
             tagType = typedArray.getInteger(R.styleable.TagView_tagType, CLASSIC);
+            tagColor = typedArray.getColor(R.styleable.TagView_tagColor, Color.BLACK);
+            tagUpperCase = typedArray.getBoolean(R.styleable.TagView_tagUpperCase, false);
+            tagBorderRadius = typedArray.getInteger(R.styleable.TagView_tagBorderRadius, BORDER_RADIUS_DEFAULT);
+            tagCircleRadius = typedArray.getInteger(R.styleable.TagView_tagCircleRadius, CIRCLE_RADIUS_DEFAULT);
+            tagCircleColor = typedArray.getColor(R.styleable.TagView_tagCircleColor, CIRCLE_COLOR_DEFAULT);
+            tagTextColor = typedArray.getColor(R.styleable.TagView_tagTextColor, TEXT_COLOR_DEFAULT);
         } finally {
             typedArray.recycle();
         }
@@ -75,24 +90,51 @@ public class TagView extends TextView {
 
     private void drawClassicTag(Rect bounds, Canvas canvas) {
         setPadding(leftPadding, topPadding, rightPadding, bottomPadding);
-
         RectF formattedBounds = getBoundsForText(bounds);
-        canvas.drawRoundRect(formattedBounds, borderRadius, borderRadius, backgroundPaint);
-        setTextColor(TEXT_COLOR);
+        canvas.drawRoundRect(formattedBounds, tagBorderRadius, tagBorderRadius, backgroundPaint);
+        setTextColor(tagTextColor);
     }
 
     private void drawModernTag(Rect bounds, Canvas canvas) {
-        setPadding(leftPadding + 5, topPadding, rightPadding, bottomPadding);
+        setPadding(leftPadding * 2, topPadding, rightPadding, bottomPadding);
         RectF formattedBounds = getBoundsForText(bounds);
-        canvas.drawRoundRect(formattedBounds, borderRadius, borderRadius, backgroundPaint);
-        float xPosition = formattedBounds.left + PADDING;
+        canvas.drawRoundRect(formattedBounds, tagBorderRadius, tagBorderRadius, backgroundPaint);
+        float xPosition = formattedBounds.left + leftPadding;
         float yPosition = (formattedBounds.bottom - formattedBounds.top) / 2;
-        canvas.drawCircle(xPosition, yPosition, circleRadius, circlePaint);
-        setTextColor(TEXT_COLOR);
+        canvas.drawCircle(xPosition, yPosition, tagCircleRadius, circlePaint);
+        setTextColor(tagTextColor);
+    }
+
+    private void drawTrapeziumTag(Rect bounds, Canvas canvas) {
+        setPadding(leftPadding, topPadding, rightPadding * 3, bottomPadding);
+        RectF formattedBounds = getBoundsForText(bounds);
+        RectF rect = new RectF(formattedBounds);
+        rect.right -= rightPadding * 3;
+        float y = (rect.bottom - rect.top) / 2;
+        canvas.drawRect(rect, backgroundPaint);
+        Path trianglePath = getTrianglePath(rect, y);
+        canvas.drawPath(trianglePath, trianglePaint);
+        setTextColor(tagTextColor);
+    }
+
+    private void drawModernTrapeziumTag(Rect bounds, Canvas canvas) {
+        setPadding(leftPadding * 2, topPadding, rightPadding * 3, bottomPadding);
+        RectF formattedBounds = getBoundsForText(bounds);
+        RectF rect = new RectF(formattedBounds);
+        rect.right -= rightPadding * 3;
+        float y = (rect.bottom - rect.top) / 2;
+        canvas.drawRect(rect, backgroundPaint);
+        float xPosition = formattedBounds.left + leftPadding;
+        float yPosition = (formattedBounds.bottom - formattedBounds.top) / 2;
+        canvas.drawCircle(xPosition, yPosition, tagCircleRadius, circlePaint);
+        Path trianglePath = getTrianglePath(rect, y);
+        canvas.drawPath(trianglePath, trianglePaint);
+        setTextColor(tagTextColor);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
+        if(tagUpperCase) setText(getText().toString().toUpperCase());
         super.onDraw(canvas);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             setBackground(new TagDrawable());
@@ -102,14 +144,28 @@ public class TagView extends TextView {
     }
 
     private void init() {
-        backgroundPaint = new Paint();
-        backgroundPaint.setColor(Color.RED);
+        backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        backgroundPaint.setColor(tagColor);
 
-        circlePaint = new Paint();
-        circlePaint.setColor(Color.WHITE);
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setColor(tagCircleColor);
+
+        trianglePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        trianglePaint.setColor(tagColor);
+        trianglePaint.setStyle(Paint.Style.FILL);
     }
 
     private RectF getBoundsForText(Rect bounds) {
         return new RectF(bounds.left, bounds.top, bounds.right, bounds.bottom);
+    }
+
+    private Path getTrianglePath(RectF rect, float y) {
+        Path path = new Path();
+        path.setFillType(Path.FillType.EVEN_ODD);
+        path.moveTo(rect.right, rect.top);
+        path.lineTo(rect.right + rightPadding * 3, y);
+        path.lineTo(rect.right, rect.bottom);
+        path.lineTo(rect.right, rect.top);
+        return path;
     }
 }
