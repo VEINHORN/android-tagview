304adb98d09f36c89a1efaf081c52af319847cce
VEINHORN <mcklas@mail.ru>
2015-03-03 23:33:32 +0300

    Add TagsView class based on ViewGroup class.

---
+++ library/src/main/java/com/veinhorn/tagview/TagsView.java
@@ -0,0 +1,19 @@
+package com.veinhorn.tagview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by veinhorn on 3.3.15.
+ */
+public class TagsView extends ViewGroup {
+    private TagsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changes, int l, int t, int r, int b) {
+
+    }
+}
